012b1d70c89f2dd2fa071343f883ea7737de722a
Pankaj Sankpal <sankpal22pankaj@gmail.com>
2017-03-15 15:41:22 +0530

    Update setup.py

--- python-fp-growth-master/setup.py
+++ python-fp-growth-master/setup.py
@@ -6,8 +6,8 @@ setup(
     name='fp-growth',
     description='A pure-python implementation of the FP-growth algorithm.',
     version='0.1.3',
-    author='Eric Naeseth',
-    author_email='eric@naeseth.com',
+    author='Pankaj Sankpal',
+    author_email='sankpal22pankaj@gmail.com',
     url='http://github.com/enaeseth/python-fp-growth/',
     
     py_modules=['fp_growth']
